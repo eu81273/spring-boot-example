a76c3215aa0e75b7c5ed11872f93753082b21fe0
AHN JAE-HA <eu81273@gmail.com>
2015-09-18 13:18:39 +0900

    add json response

--- src/main/java/example/web/TestController.java
+++ src/main/java/example/web/TestController.java
@@ -1,6 +1,7 @@
 package example.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,11 +11,25 @@ import java.util.Map;
 @Controller
 public class TestController {
 
+    /*
+        @RequestMapping annotation 을 사용해서 특정 경로에 대한 response 를 지정할 수 있다.
+        @ResponseBody annotation 을 사용하면 ViewResolver 를 거치지 않고 HTTP Response Body 에 직접 쓰여지게 된다.
+        쓰여지는 값은 리턴되는 데이터 타입에 따라 MessageConverter 에서 변환을 거치게 된다.
+     */
+
     @RequestMapping("/test")
     @ResponseBody
     public String test () {
-
         return "이것은 /test 페이지입니다.";
+    }
+
+    @RequestMapping("/json/{name}")
+    @ResponseBody
+    public Person json (@PathVariable String name) {
+        Person person = new Person();
+        person.setName(name);
+        person.setAge(50);
+        return person;
     }
 
     @RequestMapping("/")
@@ -22,7 +37,6 @@ public class TestController {
         model.put("time", new Date());
         model.put("message", "Hello World");
         model.put("title", "Hello App");
-
         return "home";
     }
 }
